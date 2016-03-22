54351b477d9bf8b5e2341c5b96cb1093eaa0a1d6
Jason Yeomans <jyeo098@gmail.com>
2016-03-21 22:03:09 -0400

    add gcm base code

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
 var request = require('request'),
   express = require('express'),
   Queue = require('firebase-queue'),
-  Firebase = require('firebase');
+  Firebase = require('firebase'),
+  gcm = require('node-gcm');
 var app = express();
 
+var sender = new gcm.Sender('YOUR_API_KEY_HERE');
 
-function startQueue() {
-  console.log('Starting Firebase GCM Queue');
-  var ref = new Firebase('https://<your-firebase>.firebaseio.com/queue');
+function startInviteQueue() {
+  console.log('Starting Firebase GCM Invite Queue');
+  var ref = new Firebase('https://flickering-heat-6442.firebaseio.com/queue/gcm/invite');
   var queue = new Queue(ref, function(data, progress, resolve, reject) {
     // Read and process task data
     console.log(data);
+    progress(10);
 
-    // Do some work
-    progress(50);
+    if (typeof data.groupName !== 'undefined' && typeof data.inviter !== 'undefined' && typeof data.inviter !== 'undefined') {
 
-    // Finish the task asynchronously
-    setTimeout(function() {
-      resolve();
-    }, 1000);
+      var message = new gcm.Message({
+        collapseKey: 'invite',
+        priority: 'high',
+        contentAvailable: true,
+        delayWhileIdle: true,
+        timeToLive: 3,
+        restrictedPackageName: "io.yeomans.echelon",
+        dryRun: true,
+        data: {
+          groupName: data.groupName,
+          inviter: data.inviter,
+          invitee: data.invitee
+        },
+        notification: {
+          title: 'Echelon Invite',
+          icon: "ic_launcher",
+          body: "This is a notification that will be displayed ASAP."
+        }
+      });
+
+      var regTokens = ['YOUR_REG_TOKEN_HERE'];
+
+      progress(50);
+
+      sender.send(message, {
+        registrationTokens: regTokens
+      }, function(err, response) {
+        if (err) {
+          console.error(err);
+          reject();
+        } else {
+          console.log(response);
+          resolve();
+        }
+      });
+    } else {
+      reject();
+    }
   });
 }
-
 
 app.get('/spotify-auth/', function(req, res) {
   res.send('This api is meant to be accessed using a POST request.');
@@ -45,5 +80,5 @@ app.post('/spotify-auth/', function(req, res) {
 
 app.listen(3000, function() {
   console.log('Echelon API listening on port 3000!');
-  startQueue();
+  startInviteQueue();
 });
